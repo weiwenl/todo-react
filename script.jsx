8069476d0a7d0733e943c5299717e06334f0e2e8
weiwenl <weiwenl.justin@gmail.com>
2018-11-12 09:29:59 +0800

    validate for length

--- script.jsx
+++ script.jsx
@@ -7,29 +7,37 @@ class List extends React.Component {
 
   state = {
     list : [],
-    word : ""
+    word : "",
+    error: ""
   }
 
   changeHandler(event){
     this.setState({word:event.target.value});
     console.log("change", event.target.value);
-
+    //creates a length validation on the input
   }
   //takes what is in the input; removes it and push it into the list
   clickHandler(event){
-    this.setState({list:[...this.state.list, this.state.word]});
-    console.log(this.state.list);
+    if( this.state.word <= 5 ){
+      this.setState({error:"input too short"});
+      console.log(this.state.error);
+    }
+    else{
+      this.setState({list:[...this.state.list, this.state.word]});
+      console.log(this.state.list);
+    }
   }
 
   render() {
     //renders the list
-    let contents = this.state.list.map(item => {
-                    return <li>{item}</li>
+    let contents = this.state.list.map((item, index) => {
+                    return <li key={index}>{item}</li>
     });
       console.log("rendering");
       return (
         <div className="list">
           <input onChange={this.changeHandler} value={this.state.word} />
+          <p>{this.state.error}</p>
           <button onClick={this.clickHandler} > Add Item </button>
           <ul>
             {contents}
